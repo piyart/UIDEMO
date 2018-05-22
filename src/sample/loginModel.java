815cd831431a0fcaa15696a201ecce34d8d2af67
piyarat <tangpiyarat23@gmail.com>
2018-05-22 16:45:55 +0700

    loginModel

---
+++ src/sample/loginModel.java
@@ -0,0 +1,56 @@
+package sample;
+
+import dbUtil.dbConnection;
+
+import java.sql.*;
+
+public class loginModel {
+
+    Connection connection;
+    //constructor
+    public loginModel(){
+       try {
+           this.connection = dbConnection.getConnection();
+       }catch (SQLDataException e){
+           e.printStackTrace();
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+        if (this.connection == null) System.exit(1);
+
+    }
+    public boolean isDatabaseConnection(){
+        return this.connection != null;
+    }
+
+    public boolean isLogin (String username, String password) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from admin where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,username);
+            pr.setString(2,password);
+
+            rs = pr.executeQuery();
+            if (rs.next()){
+                return true;
+
+            }
+            return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally {
+            pr.close();
+            rs.close();
+        }
+    }
+
+
+
+
+}//class
